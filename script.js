f314f557d1c807e1574f6d9f9e23ce32eabff37f
lsidarto <63007279+lsidarto@users.noreply.github.com>
2021-07-24 02:05:40 +0800

    landing page done!!!!!!

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-var vid = document.getElementById("vid");
-vid.playbackRate = 0.6;
-
+// var vid = document.getElementById("vid");
+// vid.playbackRate = 0.6;
+// gsap.registerPlugin(, DrawSVGPlugin, MotionPathPlugin);
 gsap.registerPlugin(MotionPathPlugin);
 
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
 
 gsap.to(".b1", {
 duration: 3, 
@@ -40,3 +43,61 @@ motionPath:{
     alignOrigin: [0.5, 0.5]
 }
 });
+
+console.clear();
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".b4", {
+    x: w,
+    duration: 4,
+    // autoAlpha:0,
+    scale: 0.8,
+    scrollTrigger: {
+        trigger:".b4",
+        start: "center 90%",
+        end: "center top",
+        toggleActions: "restart pause resume reset",
+        // markers: true,
+    }, motionPath:{
+        path: ".path4",
+        align: ".path4",
+        autoRotate: 90,
+        alignOrigin: [0.5, 0.5]
+    }
+    });
+
+// const path = document.querySelector('#longpath');
+// const circle = document.querySelector('.circle');
+
+// let pathPosition = path.getBoundingClientRect();
+// let documentPosition = document.body.getBoundingClientRect();
+// const pathTotalLength = path.getTotalLength();
+
+// function positionElements() {
+// 		// SVG passes center of screen
+// 		const relativePageOffset = -pathPosition.top +
+// 					(window.pageYOffset + window.innerHeight * .5);
+		
+// 		const pointPercentage = relativePageOffset / pathPosition.height;
+// 		const pointOnPath = pointPercentage * pathTotalLength;
+// 		const pathPoint = path.getPointAtLength(pointOnPath);
+
+// 		circle.style.transform = `translate(
+// 			${ pathPosition.left + pathPoint.x }px,
+// 			${ pathPosition.top + pathPoint.y }px
+// 		)`;
+// }
+
+// window.addEventListener('scroll', () => {
+// 	positionElements();
+// })
+
+// window.addEventListener('resize', () => {
+// 	pathPosition = path.getBoundingClientRect();
+// 	documentPosition = document.body.getBoundingClientRect();
+	
+// 	positionElements();
+// });
+
+// positionElements();
