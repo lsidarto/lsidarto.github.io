c7f66d646cb7b7dc78085d0bff68fd72a5f84521
lsidarto <63007279+lsidarto@users.noreply.github.com>
2021-07-25 23:22:15 +0800

    portfolio page start

--- script.js
+++ script.js
@@ -23,103 +23,98 @@ function nav_bg() {
     // }
 }
 
-gsap.registerPlugin(MotionPathPlugin);
 
-var w = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
+if ($("body#portfolio").length > 0) {
 
-gsap.to(".b1", {
-duration: 3, 
-repeat: -1,
-repeatDelay: 2,
-ease: "power1.inOut",
-motionPath:{
-    path: ".path1",
-    align: ".path1",
-    autoRotate: 90,
-    alignOrigin: [0.5, 0.5]
+    var coll = document.getElementsByClassName("card");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+        content.style.display = "none";
+        } else {
+        content.style.display = "block";
+        }
+    });
+    }
 }
-});
-gsap.to(".b2", {
-duration: 5, 
-repeat: -1,
-repeatDelay: 3,
-ease: "power1.inOut",
-motionPath:{
-    path: ".path2",
-    align: ".path2",
-    autoRotate: 90,
-    alignOrigin: [0.5, 0.5],
-}
-});
-gsap.to(".b3", {
-duration: 4, 
-repeat: -1,
-repeatDelay: 4,
-ease: "power1.inOut",
-motionPath:{
-    path: ".path3",
-    align: ".path3",
-    autoRotate: 90,
-    alignOrigin: [0.5, 0.5]
-}
-});
 
-console.clear();
+// birds: only run on index page
+if ($("body#index").length > 0) {
 
-gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(MotionPathPlugin);
 
-gsap.to(".b4", {
-    x: w,
-    duration: 6,
-    // autoAlpha:0,
-    scrollTrigger: {
-        trigger:".b4",
-        start: "center 90%",
-        end: "center top",
-        toggleActions: "restart pause resume reset",
-        // markers: true,
-    }, motionPath:{
-        path: ".path4",
-        align: ".path4",
+    var w = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+    gsap.to(".b1", {
+    duration: 3, 
+    repeat: -1,
+    repeatDelay: 2,
+    ease: "power1.inOut",
+    motionPath:{
+        path: ".path1",
+        align: ".path1",
         autoRotate: 90,
         alignOrigin: [0.5, 0.5]
-    }
-    });
+    }});
+    gsap.to(".b2", {
+    duration: 4, 
+    repeat: -1,
+    repeatDelay: 4,
+    ease: "power1.inOut",
+    motionPath:{
+        path: ".path2",
+        align: ".path2",
+        autoRotate: 90,
+        alignOrigin: [0.5, 0.5],
+    }});
+    gsap.to(".b3", {
+    duration: 4, 
+    repeat: -1,
+    repeatDelay: 4,
+    ease: "power1.inOut",
+    motionPath:{
+        path: ".path3",
+        align: ".path3",
+        autoRotate: 90,
+        alignOrigin: [0.5, 0.5]
+    }});
+    gsap.to(".b4", {
+        duration: 8, 
+        repeat: -1,
+        repeatDelay: 1,
+        ease: "power1.inOut",
+        motionPath:{
+            path: ".path4",
+            align: ".path4",
+            autoRotate: 90,
+            alignOrigin: [0.5, 0.5]
+    }});
 
-// const path = document.querySelector('#longpath');
-// const circle = document.querySelector('.circle');
+    console.clear();
 
-// let pathPosition = path.getBoundingClientRect();
-// let documentPosition = document.body.getBoundingClientRect();
-// const pathTotalLength = path.getTotalLength();
+    gsap.registerPlugin(ScrollTrigger);
 
-// function positionElements() {
-// 		// SVG passes center of screen
-// 		const relativePageOffset = -pathPosition.top +
-// 					(window.pageYOffset + window.innerHeight * .5);
-		
-// 		const pointPercentage = relativePageOffset / pathPosition.height;
-// 		const pointOnPath = pointPercentage * pathTotalLength;
-// 		const pathPoint = path.getPointAtLength(pointOnPath);
-
-// 		circle.style.transform = `translate(
-// 			${ pathPosition.left + pathPoint.x }px,
-// 			${ pathPosition.top + pathPoint.y }px
-// 		)`;
-// }
-
-// window.addEventListener('scroll', () => {
-// 	positionElements();
-// })
-
-// window.addEventListener('resize', () => {
-// 	pathPosition = path.getBoundingClientRect();
-// 	documentPosition = document.body.getBoundingClientRect();
-	
-// 	positionElements();
-// });
-
-// positionElements();
-
+    gsap.to(".b0", {
+        x: w,
+        duration: 6,
+        scrollTrigger: {
+            trigger:".b0",
+            start: "center 90%",
+            end: "center top",
+            toggleActions: "restart pause resume reset",
+            // markers: true,
+        }, motionPath:{
+            path: ".path0",
+            align: ".path0",
+            autoRotate: 90,
+            alignOrigin: [0.5, 0.5]
+        }
+        });
+}
