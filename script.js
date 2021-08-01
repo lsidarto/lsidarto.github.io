29b6306fd07ecd44d989e2346cd829b7d64b776e
lsidarto <63007279+lsidarto@users.noreply.github.com>
2021-08-02 02:05:19 +0800

    contact page, card smoothing

--- script.js
+++ script.js
@@ -34,14 +34,9 @@ if ($("body#portfolio").length > 0) {
 
     for (var i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
-        // this.classList.toggle("active");
         
         var content = this.children[1];
-        if (content.style.display === "none") {
-        content.style.display = "block";
-        } else {
-        content.style.display = "none";
-        }
+        content.classList.toggle("active");
     });
     }
 }
